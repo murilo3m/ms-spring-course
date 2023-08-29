378be7b5365ea20be47365bc9fee5ceb35a027f1
Murilo Maldonado Mendes <murilomaldonadom@gmail.com>
2023-08-28 21:12:33 -0300

    hr-worker comunication with RestTemplate

--- hr-payroll/src/main/java/com/murilo3m/msspringcourse/services/PaymentService.java
+++ hr-payroll/src/main/java/com/murilo3m/msspringcourse/services/PaymentService.java
@@ -1,11 +1,29 @@
 package com.murilo3m.msspringcourse.services;
 
 import com.murilo3m.msspringcourse.entities.Payment;
+import com.murilo3m.msspringcourse.entities.Worker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentService {
+
+    @Value("${hr-worker.host}")
+    private String workerHost;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     public Payment getPayment(long workerId, int days){
-        return new Payment("Bob", 100.0, days);
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("id", String.valueOf(workerId));
+        Worker worker = restTemplate.getForObject(workerHost.concat("/workers/{id}"), Worker.class, uriVariables);
+
+        return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
