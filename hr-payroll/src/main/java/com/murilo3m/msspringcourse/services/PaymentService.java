719cfa2d7b441843ac48b0b46839dbf29b3d1e78
Murilo Maldonado Mendes <murilomaldonadom@gmail.com>
2023-08-28 21:22:27 -0300

    hr-worker comunication with FeignClient

--- hr-payroll/src/main/java/com/murilo3m/msspringcourse/services/PaymentService.java
+++ hr-payroll/src/main/java/com/murilo3m/msspringcourse/services/PaymentService.java
@@ -2,6 +2,7 @@ package com.murilo3m.msspringcourse.services;
 
 import com.murilo3m.msspringcourse.entities.Payment;
 import com.murilo3m.msspringcourse.entities.Worker;
+import com.murilo3m.msspringcourse.feignclients.WorkerFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -13,17 +14,12 @@ import java.util.Map;
 @Service
 public class PaymentService {
 
-    @Value("${hr-worker.host}")
-    private String workerHost;
-
     @Autowired
-    private RestTemplate restTemplate;
+    private WorkerFeignClient workerFeignClient;
 
     public Payment getPayment(long workerId, int days){
-        Map<String, String> uriVariables = new HashMap<>();
-        uriVariables.put("id", String.valueOf(workerId));
-        Worker worker = restTemplate.getForObject(workerHost.concat("/workers/{id}"), Worker.class, uriVariables);
 
+        Worker worker = workerFeignClient.findById(workerId).getBody();
         return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
